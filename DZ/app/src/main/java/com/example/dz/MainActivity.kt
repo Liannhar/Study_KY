51866010cf498427ba08dab4460ab74f1e0c8d21
Liannhar <yura.knyashev@gmail.com>
2022-10-29 21:12:59 +0300

    second version

--- DZ/app/src/main/java/com/example/dz/MainActivity.kt
+++ DZ/app/src/main/java/com/example/dz/MainActivity.kt
@@ -1,31 +1,41 @@
 package com.example.dz
 
+import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class MainActivity: AppCompatActivity()
 {
+    private var i = 0
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val blocks = CreateList().toMutableList()
-        var i=0
 
-        val rv:RecyclerView = findViewById(R.id.activity_main_rv)
+        val rv:RecyclerView = findViewById(R.id.fragment_main_rv)
         val adapter = MainAdapter(blocks)
+        val GridLayoutManager = GridLayoutManager(this,3,RecyclerView.VERTICAL,false)
+        rv.layoutManager = GridLayoutManager
         rv.adapter=adapter
-        rv.layoutManager=LinearLayoutManager(this)
-        val fab:ExtendedFloatingActionButton = findViewById(R.id.activity_main_ab)
+        val fab:ExtendedFloatingActionButton = findViewById(R.id.fragment_main_ab)
         fab.setOnClickListener()
         {
-            if (i%2==0) blocks.add(Blocks(i,Color.RED) )
-            else blocks.add(Blocks(i,Color.BLUE) )
+            if (i%2==0) {
+                blocks.add(Blocks(i,Color.RED) )
+            }
+            else
+            {
+                blocks.add(Blocks(i,Color.BLUE) )
+            }
+
+            if(getScreenOrientation()==4) rv.layoutManager = GridLayoutManager(this,4,RecyclerView.VERTICAL,false)
+            else  rv.layoutManager = GridLayoutManager(this,3,RecyclerView.VERTICAL,false)
             adapter.notifyDataSetChanged()
             i++
         }
@@ -34,5 +44,12 @@ class MainActivity: AppCompatActivity()
     private fun CreateList(): List<Blocks>
     {
         return  emptyList<Blocks>()
+    }
+    private fun getScreenOrientation(): Int {
+        return when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> 3
+            Configuration.ORIENTATION_LANDSCAPE -> 4
+            else -> 0
+        }
     }
 }
